f035f860e359efb9778c13ca4534ea746b39e908
Ruud Laan <ruud@yard.nl>
2018-03-13 11:54:29 +0100

    added Plugin index file

---
+++ pdc-base.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       PDC Base
+ * Plugin URI:        https://www.openwebconcept.nl/
+ * Description:       Plugin to act as foundation for other PDC related content plugins. This plugin implements actions to allow for other plugins to add and/or change Custom Posttypes, Metaboxes, Taxonomies, en Posts 2 posts relations.
+ * Version:           0.1
+ * Author:            Edwin Siebel, Ruud Laan
+ * Author URI:        https://www.yarddigital.nl/
+ * License:           GPL-3.0
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:       pdc-base
+ * Domain Path:       /languages
+ */
