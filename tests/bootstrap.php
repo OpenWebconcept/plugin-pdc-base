f16c9d7175814a5781c9cbc401347295eab8046e
Ruud Laan <ruud@yard.nl>
2018-03-13 16:16:29 +0100

    fixing initial assertions

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,16 +6,16 @@
  * @package OWC_PDC_Base
  */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
-}
-
-// Give access to tests_add_filter() function.
-require_once $_tests_dir . '/includes/functions.php';
-
-// Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+//$_tests_dir = getenv( 'WP_TESTS_DIR' );
+//if ( ! $_tests_dir ) {
+//	$_tests_dir = '/tmp/wordpress-tests-lib';
+//}
+//
+//// Give access to tests_add_filter() function.
+//require_once $_tests_dir . '/includes/functions.php';
+//
+//// Start up the WP testing environment.
+//require $_tests_dir . '/includes/bootstrap.php';
 
 /**
  * Bootstrap WordPress Mock.
