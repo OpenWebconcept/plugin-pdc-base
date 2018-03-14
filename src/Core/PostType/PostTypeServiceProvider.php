8b02d16fb1444bb7f05be0656472d26d8ca5eb06
Ruud Laan <ruud@yard.nl>
2018-03-14 14:45:56 +0100

    added taxonomies (start) added method for registering posttypes

--- src/Core/PostType/PostTypeServiceProvider.php
+++ src/Core/PostType/PostTypeServiceProvider.php
@@ -10,107 +10,43 @@ class PostTypeServiceProvider extends ServiceProvider
 	public function register()
 	{
 
-		$this->plugin->loader->addAction('init', $this, 'registerPdcItemPostType');
-
-		/**
-		 * Registering the post types and taxonomies
-		 */
-
-		//				// Examples of registering post types: http://johnbillion.com/extended-cpts/
-		//
-		//				register_extended_post_type('pdc-category', [
-		//
-		//					# Add the post type to the site's main RSS feed:
-		//					'show_in_feed' => false,
-		//
-		//					# Show all posts on the post type archive:
-		//					'archive'      => [
-		//						'nopaging' => true
-		//					],
-		//
-		//					'supports' => ['title', 'editor', 'excerpt', 'revisions', 'thumbnail'],
-		//
-		//					'show_in_rest' => true,
-		//					'rest_base'    => 'pdc-thema',
-		//					'map_meta_cap' => true
-		//
-		//				], [
-		//
-		//					# Override the base names used for labels:
-		//					'singular' => 'PDC thema',
-		//					'plural'   => 'PDC themas',
-		//					'slug'     => 'pdc-thema'
-		//
-		//				]);
-		//
-		//				register_extended_post_type('pdc-subcategory', [
-		//
-		//					# Add the post type to the site's main RSS feed:
-		//					'show_in_feed' => false,
-		//
-		//					# Show all posts on the post type archive:
-		//					'archive'      => [
-		//						'nopaging' => true
-		//					],
-		//
-		//					'supports' => ['title', 'editor', 'excerpt', 'revisions', 'thumbnail', 'page-attributes'],
-		//
-		//					'show_in_rest' => true,
-		//					'hierarchical' => true,
-		//					'rest_base'    => 'pdc-subthema',
-		//					'map_meta_cap' => true
-		//
-		//				], [
-		//
-		//					# Override the base names used for labels:
-		//					'singular' => 'PDC subthema',
-		//					'plural'   => 'PDC subthemas',
-		//					'slug'     => 'pdc-subthema'
-		//
-		//				]);
-		//			}
-		//
-		//			add_action('init', 'register_extended_post_type', 3);
-		//		}
-
+		$this->plugin->loader->addAction('init', $this, 'registerPostTypes');
+		$this->plugin->loader->addAction('init', $this, 'registerTaxonomies');
 	}
 
 	/**
-	 * register PDC-item custom posttype.
+	 * register custom posttypes.
 	 */
-	public function registerPdcItemPostType()
+	public function registerPostTypes()
 	{
 
 		if ( function_exists('register_extended_post_type') ) {
 
-			// Examples of registering post types: http://johnbillion.com/extended-cpts/
-			register_extended_post_type('pdc-item', [
+			$postTypes = apply_filters('owc_pdc_base_posttypes', $this->plugin->config->get('posttypes'));
 
-				# Add the post type to the site's main RSS feed:
-				'show_in_feed' => false,
+			foreach ( $postTypes as $postTypeName => $postType ) {
 
-				# Show all posts on the post type archive:
-				'archive'      => [
-					'nopaging' => true
-				],
+				// Examples of registering post types: http://johnbillion.com/extended-cpts/
+				register_extended_post_type($postTypeName, $postType['args'], $postType['names']);
+			}
+		}
+	}
 
-				'supports' => ['title', 'editor', 'thumbnail', 'excerpt', 'revisions', 'custom-fields'],
+	/**
+	 * register taxonomies.
+	 */
+	public function registerTaxonomies()
+	{
 
-				'show_in_rest'          => true,
-				'rest_base'             => 'pdc-item',
-				'rest_controller_class' => 'OWC_PDC_Base\\REST_PDC_Posts_Controller',
-				'capability_type'       => 'pdc_post',
-				'map_meta_cap'          => true
+		if ( function_exists('register_extended_taxonomies') ) {
 
-			], [
+			$taxonomies = apply_filters('owc_pdc_base_taxonomies', $this->plugin->config->get('taxonomies'));
 
-				# Override the base names used for labels:
-				'singular' => 'PDC item',
-				'plural'   => 'PDC items',
-				'slug'     => 'pdc-item'
+			foreach ( $taxonomies as $taxonomyName => $taxonomy ) {
 
-			]);
-
+				// Examples of registering taxonomies: http://johnbillion.com/extended-cpts/
+				register_extended_taxonomy($taxonomyName, $taxonomy['objectTypes'], $taxonomy['args'], $taxonomy['names']);
+			}
 		}
 	}
 }
