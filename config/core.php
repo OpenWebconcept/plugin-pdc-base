bb1fea610a1c9d49f44358bb2a441058f28f832f
Ruud Laan <ruud@yard.nl>
2018-03-26 12:47:07 +0200

    put SettingServiceProvider on top, to ensure availability of $plugin->settings for other purposes

--- config/core.php
+++ config/core.php
@@ -9,12 +9,12 @@ return [
         /**
          * Global providers.
          */
+	    OWC_PDC_Base\Core\Settings\SettingsServiceProvider::class,
 	    OWC_PDC_Base\Core\PostType\PostTypeServiceProvider::class,
 	    OWC_PDC_Base\Core\Taxonomy\TaxonomyServiceProvider::class,
 	    OWC_PDC_Base\Core\PostsToPosts\PostsToPostsServiceProvider::class,
 	    OWC_PDC_Base\Core\Metabox\MetaboxServiceProvider::class,
 	    OWC_PDC_Base\Core\RestApi\RestApiServiceProvider::class,
-	    OWC_PDC_Base\Core\Settings\SettingsServiceProvider::class,
 	    /**
          * Providers specific to the admin.
          */
