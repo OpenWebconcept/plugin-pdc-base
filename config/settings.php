339c9cf33e53f329792399af3bfad733276e388c
Ruud Laan <ruud@yard.nl>
2018-04-10 15:31:01 +0200

    improved handling of settings pages and its ways to add additional pages/tabs/fields

--- config/settings.php
+++ config/settings.php
@@ -4,14 +4,11 @@ return [
 
 	'base' => [
 		'id'             => 'general',
-		'title'          => __('PDC basis plugin instellingen','pdc-base'),
+		'title'          => _x('Portal', 'PDC instellingen subpagina', 'pdc-base'),
 		'settings_pages' => '_owc_pdc_base_settings',
+		'tab' => 'base',
 		'fields'         => [
 			'portal' => [
-				'heading'       => [
-					'type' => 'heading',
-					'name' => __('Portal','pdc-base'),
-				],
 				'portal_url'    => [
 					'name' => __('Portal URL','pdc-base'),
 					'desc' => __('URL inclusief http(s)://','pdc-base'),
@@ -19,7 +16,7 @@ return [
 					'type' => 'text'
 				],
 				'pdc_item_slug' => [
-					'name' => __('PDC item slug','pdc-base'),
+					'name' => __('Portal PDC item slug','pdc-base'),
 					'desc' => __('URL van de PDC items in de portal, bv "onderwerp"','pdc-base'),
 					'id'   => 'setting_portal_pdc_item_slug',
 					'type' => 'text'
--- config/settings_pages.php
+++ config/settings_pages.php
@@ -4,11 +4,16 @@ return [
 
 	'base' => [
 
-		'id'          => '_owc_pdc_base_settings',
-		'option_name' => '_owc_pdc_base_settings',
-		'menu_title'  => __('PDC instellingen pagina','pdc-base'),
-		'parent'      => '',
-		'icon_url'    => 'dashicons-admin-settings',
-		'position'    => 9
+		'id'            => '_owc_pdc_base_settings',
+		'option_name'   => '_owc_pdc_base_settings',
+		'menu_title'    => __('PDC instellingen', 'pdc-base'),
+		'icon_url'      => 'dashicons-admin-settings',
+		'submenu_title' => _x('Basis', 'PDC instellingen subpagina', 'pdc-base'),
+		'position'      => 9,
+		'columns'       => 1,
+		'submit_button' => _x('Opslaan', 'PDC instellingen subpagina', 'pdc-base'),
+		'tabs'          => array(
+			'base'        => _x('Algemeen', 'PDC instellingen tab', 'pdc-base')
+		)
 	]
 ];
