65d80b7a652908a3efb2b0502796df92ac612a9b
Ruud Laan <ruud@yard.nl>
2018-04-30 11:55:35 +0200

    bump version to 1.0

--- pdc-base.php
+++ pdc-base.php
@@ -3,7 +3,7 @@
  * Plugin Name:       PDC Base
  * Plugin URI:        https://www.openwebconcept.nl/
  * Description:       Plugin to act as foundation for other PDC related content plugins. This plugin implements actions to allow for other plugins to add and/or change Custom Posttypes, Metaboxes, Taxonomies, en Posts 2 posts relations.
- * Version:           0.1
+ * Version:           1.0
  * Author:            Edwin Siebel, Ruud Laan
  * Author URI:        https://www.yarddigital.nl/
  * License:           GPL-3.0
