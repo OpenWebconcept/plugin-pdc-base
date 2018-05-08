296ce5f0b47440932b2dd87bf8e10462bdbc7c71
Melvin Koopmans <melvin.koopmans@gmail.com>
2018-05-08 12:16:44 +0200

    Update plugin docs

--- pdc-base.php
+++ pdc-base.php
@@ -2,11 +2,13 @@
 /**
  * Plugin Name:       PDC Base
  * Plugin URI:        https://www.openwebconcept.nl/
- * Description:       Plugin to act as foundation for other PDC related content plugins. This plugin implements actions
+ * Description:       Acts as foundation for other PDC related content plugins. This plugin implements actions
  * to allow for other plugins to add and/or change Custom Posttypes, Metaboxes, Taxonomies, en Posts 2 posts relations.
- * Version:           1.1 Author:            Edwin Siebel, Ruud Laan
+ * Version:           2.0.0
+ * Author:            Edwin Siebel, Ruud Laan, Melvin Koopmans
  * Author URI:        https://www.yarddigital.nl/
- * License:           GPL-3.0 License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * License:           GPL-3.0
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
  * Text Domain:       pdc-base
  * Domain Path:       /languages
  */
