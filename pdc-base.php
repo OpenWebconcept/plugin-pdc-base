ae7e0c2c5c52b530d6db600a44f41a8684c4ee87
Edwin Siebel <edwin@yard.nl>
2018-07-31 16:07:44 +0200

    Change: cleaner way to boot plugin

--- pdc-base.php
+++ pdc-base.php
@@ -64,8 +64,8 @@ register_uninstall_hook(__FILE__, [ Hooks::class, 'uninstallPlugin' ]);
  * This hook is called once any activated plugins have been loaded. Is generally used for immediate filter setup, or
  * plugin overrides. The plugins_loaded action hook fires early, and precedes the setup_theme, after_setup_theme, init
  * and wp_loaded action hooks.
- *
  */
-add_action('plugins_loaded', function () use ($plugin) {
-    (new Plugin(__DIR__))->boot();
+
+add_action('plugins_loaded', function() {
+    $plugin = (new Plugin(__DIR__))->boot();
 }, 10);
