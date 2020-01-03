d014e6ac23287b1c9ca4337e5a544899bc79a732
Simon <simon@yard.nl>
2020-01-03 13:18:33 +0100

    (fix): check for empty fields

--- src/Base/RestAPI/ItemFields/LinksField.php
+++ src/Base/RestAPI/ItemFields/LinksField.php
@@ -24,11 +24,12 @@ class LinksField extends CreatesFields
     public function create(WP_Post $post): array
     {
         return array_map(function ($link) {
-            $url = $link['pdc_links_url'];
-            if (empty($link['pdc_links_url'])) {
+            if(!empty($link['pdc_links_url'])){
+                $url = $link['pdc_links_url'];
+            } elseif (!empty($link['pdc_links_shortcode'])) {
                 $url = do_shortcode($link['pdc_links_shortcode']);
             }
-
+            
             return [
                 'title' => esc_attr(strip_tags($link['pdc_links_title'])),
                 'url'   => esc_url($url),
@@ -46,7 +47,7 @@ class LinksField extends CreatesFields
     private function getLinks(WP_Post $post)
     {
         return array_filter(get_post_meta($post->ID, '_owc_pdc_links_group', true) ?: [], function ($link) {
-            return (!empty($link['pdc_links_url']) or !empty($link['pdc_links_shortcode'])) && (!empty($link['pdc_links_title']));
+            return (!empty($link['pdc_links_url']) || !empty($link['pdc_links_shortcode'])) && (!empty($link['pdc_links_title']));
         });
     }
 }
