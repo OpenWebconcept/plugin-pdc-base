4e759e014de3872707d249d547fd72a9e383aa2b
Jeroen Bakker <jeroen@MacBook-Pro-van-Jeroen.local>
2022-09-01 15:02:09 +0200

    (feat): enrichment parsedown to text

--- src/Base/Repositories/Item.php
+++ src/Base/Repositories/Item.php
@@ -2,6 +2,8 @@
 
 namespace OWC\PDC\Base\Repositories;
 
+use WP_Post;
+
 /**
  * repository for the item
  */
@@ -22,4 +24,64 @@ class Item extends AbstractRepository
      * @var array $globalFields
      */
     protected static $globalFields = [];
+
+    /**
+     * Transform a single WP_Post item.
+     *
+     * @param WP_Post $post
+     *
+     * @return array
+     */
+    public function transform(WP_Post $post)
+    {
+        $data = [
+            'id'          => $post->ID,
+            'title'       => $post->post_title,
+            'slug'        => $post->post_name,
+            'content'     => $this->isAllowed($post) ? apply_filters('the_content', $this->stripSpecificBlock($post)) : "",
+            'excerpt'     => $this->isAllowed($post) ? $post->post_excerpt : "",
+            'date'        => $post->post_date,
+            'slug'        => $post->post_name,
+            'post_status' => $post->post_status,
+            'protected'   => !$this->isAllowed($post)
+        ];
+
+        $data = $this->assignFields($data, $post);
+
+        return $this->getPreferredFields($data);
+    }
+    
+    protected function stripSpecificBlock(WP_Post $post): string
+    {
+        $showSpecificInAPI = $this->showSpecificTextInApi($post);
+
+        if(! $showSpecificInAPI) {
+            return $post->post_content;
+        }
+
+        $pattern = '/(<!-- Test specific text -->)/';
+        return preg_replace_callback($pattern, function($matches){
+            return '';
+        }, $post->post_content);
+    }
+
+    protected function showSpecificTextInApi(WP_Post $post): bool
+    {
+        $group = $this->getEnrichmentGroup($post);
+
+        $show = $group['enrichment_specific_show_in_api'] ?? '';
+
+        return $show === '1' ? true : false;
+    }
+
+    protected function getEnrichmentGroup(WP_Post $post): array
+    {
+        $enrichmentGroup = get_post_meta($post->ID, '_owc_enrichment-group', true);
+        
+        if (empty($enrichmentGroup) || !is_array($enrichmentGroup)){
+            return [];
+        }
+
+        return $enrichmentGroup;
+    }
 }
