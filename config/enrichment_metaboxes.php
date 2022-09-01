1c2f8ed6dbda1d6bf9404b2fb310c1fb6c823767
Jeroen Bakker <jeroen@MacBook-Pro-van-Jeroen.local>
2022-09-01 14:59:18 +0200

    (feat): enrichment translations

--- config/enrichment_metaboxes.php
+++ config/enrichment_metaboxes.php
@@ -10,6 +10,15 @@ return [
         'priority'   => 'high',
         'autosave'   => true,
         'fields'     => [
+            'enrichtment-data' => [
+                'data   ' => [
+                    'name' => __('Last modified', 'pdc-base'),
+                    'id'   => 'enrichment_version_date',
+                    'type' => 'date',
+                    'desc' => __('This field shows the latest version.', 'pdc-base'),
+                    'readonly' => true,
+                ]
+            ],
             'enrichment-group' => [
                 'group' => [
                     'id'         => 'enrichment-group',
@@ -21,24 +30,28 @@ return [
                             'name' => __('Language', 'pdc-base'),
                             'id'   => 'enrichment_language',
                             'type' => 'text',
+                            'desc' => __('The language of the loaded text is shown here.', 'pdc-base'),
                             'readonly' => true
                         ],
                         [
-                            'name' => __('Specific Text', 'pdc-base'),
+                            'name' => __('Mandatory text', 'pdc-base'),
                             'id'   => 'enrichment_specific_text',
                             'type' => 'textarea',
+                            'desc' => __('This text needs to be placed in the content as an exact copy. Select the complete text, copy (CTRL+C) the text and paste (CTRL+V) the text in the content of the corresponding PDC item.', 'pdc-base'),
                             'readonly' => true
                         ],
                         [
-                            'name' => __('Specific text in feed', 'pdc-base'),
-                            'id'   => 'enrichment_specific_show_in_checkbox',
+                            'name' => __('Mandatory text to SDG', 'pdc-base'),
+                            'id'   => 'enrichment_specific_show_in_api',
                             'type' => 'checkbox',
+                            'desc' => __('Activate this field if the mandatory text is not part of the content but needs to be send to the SDG API.', 'pdc-base'),
                         ],
                         [
                             'name' => __('Procedure description', 'pdc-base'),
                             'id'   => 'enrichment_procedure_description',
-                            'type' => 'textarea'
-                        ]
+                            'desc' => __('This text can be modified and copied here. Be aware: After updating, this text will be reset!', 'pdc-base'),
+                            'type' => 'wysiwyg'
+                        ],
                     ]
                 ]
             ]
