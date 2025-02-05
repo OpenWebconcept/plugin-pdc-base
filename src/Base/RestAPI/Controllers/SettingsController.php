1a8777c175365ac34c79d5123ee4afd1b9dbdd67
Richard Korthuis <richardkorthuis@acato.nl>
2025-02-05 12:22:33 +0100

    (feat): add settings API endpoint

---
+++ src/Base/RestAPI/Controllers/SettingsController.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Controller which handles the (requested) settings.
+ */
+
+namespace OWC\PDC\Base\RestAPI\Controllers;
+
+use WP_REST_Request;
+use OWC\PDC\Base\Settings\SettingsPageOptions;
+
+/**
+ * Controller which handles the settings.
+ */
+class SettingsController extends BaseController
+{
+	/**
+	 * Get the settings.
+	 */
+	public function getSettings(WP_REST_Request $request): array
+	{
+		$settingsPageOptions = SettingsPageOptions::make();
+
+		$settings = [
+			'item_slug' => $settingsPageOptions->getPortalItemSlug(),
+			'theme_in_portal_url' => $settingsPageOptions->themeInPortalURL(),
+			'subtheme_in_portal_url' => $settingsPageOptions->subthemeInPortalURL(),
+			'id_in_portal_url' => $settingsPageOptions->idInPortalURL(),
+		];
+
+		return $settings;
+	}
+}
