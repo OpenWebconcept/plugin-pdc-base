fd4a03fa9c88f6498e9b107ed07bf3b31fc70ca5
Roric van Schalkwijk <roric.vanschalkwijk@yard.nl>
2025-04-17 15:46:46 +0200

    (feat): add extra filter before register extended post types

--- src/Base/PostType/PostTypeServiceProvider.php
+++ src/Base/PostType/PostTypeServiceProvider.php
@@ -36,7 +36,8 @@ class PostTypeServiceProvider extends ServiceProvider
     public function registerPostTypes()
     {
         if (function_exists('register_extended_post_type')) {
-            $this->configPostTypes = $this->plugin->config->get('posttypes', []);
+
+			$this->configPostTypes = apply_filters('owc/pdc-base/before-register-extended-post-types', $this->plugin->config->get('posttypes', []));
 
             foreach ($this->configPostTypes as $postTypeName => $postType) {
                 if ('pdc-group' === $postTypeName && ! $this->plugin->settings->useGroupLayer()) {
